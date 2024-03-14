dfa5c2b07df311309c5386649f32d9876d7a162a
kartikeya sharma <karthiksharma1411@gmail.com>
2024-03-14 22:30:21 +0530

    setting env file

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -5,15 +5,15 @@ import 'firebase/compat/storage';
 
 
 const firebaseConfig = {
-        apiKey: "AIzaSyAMOgZj-HNEdEANoWZqD6thyK7waxJ2FpU",
-        authDomain: "cycle-e5661.firebaseapp.com",
-        projectId: "cycle-e5661",
-        storageBucket: "cycle-e5661.appspot.com",
-        messagingSenderId: "763530731165",
-        appId: "1:763530731165:web:a12430deeb1ed674afbe74",
-        measurementId: "G-V45QBK2MT2"
-};
-
+        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_FIREBASE_APP_ID,
+        measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+      };
+      
 firebase.initializeApp(firebaseConfig);
 const firestore = firebase.firestore();
 const auth = firebase.auth();
